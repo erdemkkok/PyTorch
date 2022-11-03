72b33d34a3491e4cb32834cf8a8ad4891161517c
Erdem Kok <erdemkok@Erdem-MacBook-Pro.local>
2022-11-03 23:35:05 +0300

    Torcher guncelleme

---
+++ dataset_torch.py
@@ -0,0 +1,101 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+import numpy as np
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+a=training_data[8][0][0].numpy()
+b=np.array(a)
+plt.imshow(b)
+plt.show()
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,#Test data olup olmadığını belirleyen parametre
+    download=True,
+    transform=ToTensor()
+)
+
+# print(len(test_data))
+# labels_map = {
+#     0: "T-Shirt",
+#     1: "Trouser",
+#     2: "Pullover",
+#     3: "Dress",
+#     4: "Coat",
+#     5: "Sandal",
+#     6: "Shirt",
+#     7: "Sneaker",
+#     8: "Bag",
+#     9: "Ankle Boot",
+# }
+# figure = plt.figure(figsize=(8, 8))
+# cols, rows = 3, 3
+# for i in range(1, cols * rows + 1):
+#     sample_idx = torch.randint(len(training_data), size=(1,)).item()
+#     print(sample_idx)
+#     img, label = training_data[sample_idx]
+    
+#     print("id",label)
+#     figure.add_subplot(rows, cols, i)
+#     plt.title(labels_map[label])
+#     plt.axis("off")
+#     plt.imshow(img.squeeze(), cmap="gray")
+# plt.show()
+
+
+print("*0*0*0*0*0*0*0*0*0 Creating a Custom Dataset for your files")
+
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        
+        self.img_dir = img_dir
+        print("fsdg",self.img_dir)
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        print("PATH",img_path)
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+    
+image=CustomImageDataset(annotations_file='/home/erdem/Desktop/Tensor_workspace/image/labels.csv',img_dir='/home/erdem/Desktop/Tensor_workspace/image/foto')
+import numpy as np
+import cv2 
+print("TRY",image[4])
+t=image[8][0][0]
+p=image[8][0][0].numpy()
+s=np.array(p)
+#s=s.reshape(2000,900,3)
+print(s.shape)
+#print(image[0][0].numpy())
+plt.imshow(s)
+plt.show()
+
+print("*0*0*0*0*0*0*0*0*0 Preparing your data for training with DataLoaders")
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+print(train_dataloader)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
--- torcher.py
+++ torcher.py
@@ -1,4 +1,67 @@
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
 
+data = [[1, 2], [3, 4]]
+x_data = torch.tensor(data)
+
+print(x_data)
+
+np_array = np.array(data)
+print(np_array)
+x_np = torch.from_numpy(np_array)# Numpy array to torch tensor
+print(x_np)
+
+x_ones = torch.ones_like(x_data)# ones matrisi oluşturur
+print(f"Ones Tensor: \n {x_ones} \n")
+
+x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
+print(f"Random Tensor: \n {x_rand} \n")
+
+shape = (2,2)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+tensor = torch.rand(3, 4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+
+tensor = torch.ones(4, 4)
+tensor[1] = 0 #1. satır 0 olur
+print(tensor)
+
+
+t1 = torch.cat([tensor, tensor, tensor], dim=1) #sütun olarak tensorleri birleştirir Yanyana
+print(t1)
+
+# This computes the element-wise product
+print(f"tensor.mul(tensor) \n {tensor.mul(tensor)} \n")
+# Alternative syntax:
+print(f"tensor * tensor \n {tensor * tensor}") # iki tane tensör çarpımıdır.
+
+print(tensor, "\n")
+tensor.add_(5) # Heryere 5 ekler
+print(tensor)
+
+print("*****************************************")
+
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
+
+n = np.ones(5)
+t = torch.from_numpy(n)
+
+sample_idx = torch.randint(3, size=(1,)) # 1, boyutunda max değeri 3 olan random sayılarla diziyi doldur
+print("samp",sample_idx)
